8ac89977cdaa2a1b2002df7655469cbd31450613
xSnakkes <102847435+xSnakkes@users.noreply.github.com>
2023-02-21 05:03:50 +0200

    add 2 project

---
+++ QWERy/script/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Navigation arrows
+    navigation: {
+      nextEl: '.button-next',
+      prevEl: '.button-prev',
+    },
+    speed: 1200,
+    loop: true
+  
+  });
